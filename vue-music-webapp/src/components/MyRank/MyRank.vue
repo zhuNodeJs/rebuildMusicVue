84a931b5cceb09d06cbe384de6fa921b7d655522
357095 <zhushunli001@deppon.com>
2018-10-30 20:55:47 +0800

    loading

--- vue-music-webapp/src/components/MyRank/MyRank.vue
+++ vue-music-webapp/src/components/MyRank/MyRank.vue
@@ -1,13 +1,53 @@
 <template>
-  <div>
-    MyRank
+  <div class="my-rank" ref="rankRef">
+    <my-scroll class="toplist" ref="scrollRef" :data='toplist'>
+      <ul>
+        <li class="item" v-for="(item, index) in toplist" :key="index" @click="selectItem(item)">
+          <!-- 左图 -->
+          <div class="icon">
+            <img width="100" height="100" v-lazy='item.picUrl' @load="loadImg">
+          </div>
+
+          <!-- 右歌 -->
+          <ul class="songlist">
+            <li class="song" v-for="(song, index) in item.songList" :key="index">
+              <span>{{index+1}}</span>
+              <span>{{song.songname}}</span>
+              <span class="singername">- {{song.singername}}</span>
+            </li>
+          </ul>
+        </li>
+      </ul>
+
+      <!-- loading组件 -->
+      <div v-show="!toplist.length" class="loading-container">
+        <my-loading></my-loading>
+      </div>
+    </my-scroll>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'MyRank'
-  }
+import MyLoading from '@/components/Base/MyLoading/MyLoading'
+import MyScroll from '@/components/Base/MyScroll/MyScroll'
+export default {
+    name: 'MyRank',
+    data() {
+      return {
+
+      }
+    },
+    props: {},
+    watch: {},
+    filters: {},
+    methods: {
+
+    },
+    components: {
+      MyScroll,
+      MyLoading
+    }
+}
 </script>
 
 <style lang='scss' scoped>
